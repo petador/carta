e802563c5600b9582823e323574f198d570b26fa
petador <158123546+petador@users.noreply.github.com>
2024-01-30 05:20:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 window.onload = function() {
-  // Aquest és el proxy de CORS Anywhere
   var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  
-  // Aquesta és la URL del teu Apps Script
-  var targetUrl = "https://script.google.com/macros/s/AKfycbxCmBlQYwzA1NTwnskRdxTHtZ3Efmz72ssvsyON-1uPPeCmh3UF-OnSyVfkK-NOtGMkRw/exec";
-  
+  var targetUrl = 'https://script.google.com/macros/s/AKfycbxCmBlQYwzA1NTwnskRdxTHtZ3Efmz72ssvsyON-1uPPeCmh3UF-OnSyVfkK-NOtGMkRw/exec';
+
   fetch(proxyUrl + targetUrl)
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('response').innerText = JSON.stringify(data);
+    .then(response => response.text()) // Canvia a .text() per obtenir la resposta com a text pla.
+    .then(text => {
+      console.log(text); // Imprimeix el text a la consola per a depuració.
+      // Ara pots veure què estàs rebent realment i decidir com procedir.
     })
     .catch(error => {
       console.error('Error:', error);
     });
 };
+
