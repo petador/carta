0ad8a337889dbcd5867124ff5aea7574fb3ffef3
petador <158123546+petador@users.noreply.github.com>
2024-01-30 04:55:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-
 window.onload = function() {
-    fetch('https://script.google.com/a/macros/onmedic.es/s/AKfycbzNVIYbJpVLFmyDRbGd7oUnKzgwzIYiJ3enu6UDozjyPOBmMeRzd8yH7vCE_ry-qryXYw/exec', {
-        method: 'GET' // o 'POST', depenent del teu backend
-    })
+  // Aquest és el proxy de CORS Anywhere
+  var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  
+  // Aquesta és la URL del teu Apps Script
+  var targetUrl = "https://script.google.com/a/macros/onmedic.es/s/AKfycbzNVIYbJpVLFmyDRbGd7oUnKzgwzIYiJ3enu6UDozjyPOBmMeRzd8yH7vCE_ry-qryXYw/exec";
+  
+  fetch(proxyUrl + targetUrl)
     .then(response => response.json())
     .then(data => {
-        document.getElementById('response').innerText = JSON.stringify(data);
+      document.getElementById('response').innerText = JSON.stringify(data);
     })
     .catch(error => {
-        console.error('Error:', error);
+      console.error('Error:', error);
     });
 };
